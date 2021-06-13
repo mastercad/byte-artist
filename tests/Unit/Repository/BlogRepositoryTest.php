d6ecc3e35e054ad291b19f759151148a24828eca
MasterCAD <andreas.kempe@byte-artist.de>
2021-06-13 14:20:23 +0200

    load test in RespositoryTest because loadFixtures does not work

--- tests/Unit/Repository/BlogRepositoryTest.php
+++ tests/Unit/Repository/BlogRepositoryTest.php
@@ -5,17 +5,19 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Repository;
 
 use App\DataFixtures\UserFixtures;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BlogRepositoryTest extends WebTestCase
 {
     public function testFindLatest()
     {
+        $this->markSkippedForMissingDependecy("loadFixtures is unknown but it should from WebTestCase! Have to Fix it");
+        /*
         $this->loadFixtures(
             [
                 UserFixtures::class
             ]
         );
+        */
     }
 }
